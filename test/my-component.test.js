c5ae1243c712e3766bceeb9af40b03f794402073
Demetrio Carvalho <resource.dcarvalho@bv.com.br>
2019-07-17 19:38:23 -0300

    trying to use @open-wc/testing

--- test/my-component.test.js
+++ test/my-component.test.js
@@ -1,10 +1,19 @@
+import {
+  html,
+  fixture,
+  expect,
+} from '../node_modules/@open-wc/testing';
 import { MyComponent } from "../src/my-component.js";
-import { TestUtils } from "./test-utils.js";
+//import { TestUtils } from "./test-utils.js";
 
 describe("simple test", () => {
-  it("first test", async () => {
+  /*it("first test", async () => {
     const { shadowRoot } = await TestUtils.render(MyComponent);
     //expect(shadowRoot.querySelector("#mydiv")).toBeTruthy();
+  });*/
+  it("test", async () => {
+    const el = await fixture("<my-component></my-component>");
+    expect(el.success).to.be.false;
   });
 });
 /*
