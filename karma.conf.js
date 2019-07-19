4f37c71d20bf26c828cf82c57b2b531335a85d27
Demetrio Carvalho <resource.dcarvalho@bv.com.br>
2019-07-19 14:21:30 -0300

    added fixture

--- karma.conf.js
+++ karma.conf.js
@@ -1,22 +1,25 @@
 module.exports = function(config) {
-    config.set({
-      basePath: "",
-      frameworks: ["jasmine"],
-      files: [
-        "node_modules/@webcomponents/webcomponentsjs/webcomponents-bundle.js",
-        { pattern: "**/*.test.js", type: "module", included: true },
-        { pattern: "**/*.js", type: "module", included: false }
-      ],
-      exclude: [],
-      preprocessors: {},
-      reporters: ["spec"],
-      port: 9876,
-      colors: true,
-      logLevel: config.LOG_INFO,
-      autoWatch: true,
-      browsers: ["Chrome"],
-      singleRun: true,
-      concurrency: Infinity
-    });
-  };
-  
+  config.set({
+    basePath: "",
+    frameworks: ["fixture","jasmine"],
+    files: [
+      "node_modules/@webcomponents/webcomponentsjs/webcomponents-bundle.js",
+      {
+        pattern: "**/*.test-without-openwc.js",
+        type: "module",
+        included: true
+      },
+      { pattern: "**/*.js", type: "module", included: false }
+    ],
+    exclude: [],
+    preprocessors: {},
+    reporters: ["spec"],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ["Chrome"],
+    singleRun: true,
+    concurrency: Infinity
+  });
+};
