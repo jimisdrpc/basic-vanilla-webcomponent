bc5e32b176a33490aa27efa64df732199a14c4c6
Demetrio Carvalho <resource.dcarvalho@bv.com.br>
2019-07-19 19:18:34 -0300

    edited suites in order to try run only on file

--- wct.conf.js
+++ wct.conf.js
@@ -1,12 +1,11 @@
 module.exports = {
-    npm:true,
-    verbose: true,
-    plugins: {
-        local: {
-            browsers: ["chrome"]
-        }
-    },
-    suites:
-        ["test/hello-world-test.html"],
-    wctPackageName: "wct-browser-legacy"
+  npm: true,
+  verbose: true,
+  plugins: {
+    local: {
+      browsers: ["chrome"]
+    }
+  },
+  suites: "test/hello-world-test.html",
+  wctPackageName: "wct-browser-legacy"
 };
